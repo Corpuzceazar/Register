8c04539425c6ebf830fe3addba9a1ac1dbcdb143
Ceazarcorpuz <bccorpuz.wup@gmail.com>
2024-03-05 16:13:17 +0800

    1st Register

---
+++ Register.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+console.error("ERROR HAPPENED!");
+console.warn("Warning!");
+
+let name = "Bill";
+lastname = "Corpuz";
+age = 19,
+isProf = true,
+hobbies = [Apex, apex, apex];
+run = function(){
+    console.log("Im running.");
+};
+
+console.log(name, age, isProf, hobbies, run)
+console.log(typeof run);
+console.table(hobbies)
+console.log("My name is " + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
